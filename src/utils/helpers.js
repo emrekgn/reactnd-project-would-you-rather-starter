f30f03a124223045d6b722550587d88718f14efc
emrekgn <emrekaganakkaya@gmail.com>
2020-10-13 17:41:03 +0300

    feat: Added helper func to format represantational value

---
+++ src/utils/helpers.js
@@ -0,0 +1,15 @@
+export function formatQuestion (question, user) {
+  const { name, avatarURL } = user
+
+  return {
+    ...question,
+    username: name,
+    avatar: avatarURL,
+  }
+}
+
+export function formatDate (timestamp) {
+  const d = new Date(timestamp)
+  const time = d.toLocaleTimeString('en-US')
+  return time.substr(0, 5) + time.slice(-2) + ' | ' + d.toLocaleDateString()
+}
