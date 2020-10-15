71f51833c8d4e37a53a6a5d1857d420c0d5603ad
emrekgn <emrekaganakkaya@gmail.com>
2020-10-15 20:17:13 +0300

    refactor: Remove unused args

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -24,8 +24,7 @@ class Questions extends Component {
   }
 }
 
-function mapStateToProps({ authedUser, questions, users}, props) {
-
+function mapStateToProps({ authedUser, questions, users}) {
   const answeredIdArr = users[authedUser].answers !== null ? Object.keys(users[authedUser].answers) : null
   const answeredQuestions = answeredIdArr !== null 
     ? Object.entries(questions).filter(([qid, q]) => answeredIdArr.includes(qid)).map(([qid, q]) => q).sort((a, b) => b.timestamp - a.timestamp)
