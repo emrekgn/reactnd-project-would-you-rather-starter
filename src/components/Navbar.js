4dd0e0a8d9aaaa0d67310c4e46d5a644e7fee04b
emrekgn <emrekaganakkaya@gmail.com>
2020-10-16 08:35:17 +0300

    refactor: Make NavBar a function component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,42 @@
-import React, { Component, Fragment } from 'react'
+import React, { Fragment, useRef } from 'react'
 import { Menubar } from 'primereact/menubar'
 import { Button } from 'primereact/button'
 import { Toast } from 'primereact/toast'
-import { withRouter } from 'react-router-dom'
+import { withRouter, useHistory } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setAuthedUser } from '../actions/authedUser'
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props)
-    this.items = [
-      {
-        label: 'Home',
-        icon: 'pi pi-fw pi-home',
-        command: (e) => {
-          this.props.history.push('/')
-        }
-      },
-      {
-        label: 'New Question',
-        icon: 'pi pi-plus',
-        command: (e) => {
-          this.props.history.push('/add')
-        }
-      },
-      {
-        label: 'Leaderboard',
-        icon: 'pi pi-list',
-        command: (e) => {
-          this.props.history.push('/leaderboard')
-        }
+const Navbar = (props) => {
+  const toastBC = useRef(null)
+  const items = [
+    {
+      label: 'Home',
+      icon: 'pi pi-fw pi-home',
+      command: (e) => {
+        handleRedirect('/')
       }
-    ]
+    },
+    {
+      label: 'New Question',
+      icon: 'pi pi-plus',
+      command: (e) => {
+        handleRedirect('/add')
+      }
+    },
+    {
+      label: 'Leaderboard',
+      icon: 'pi pi-list',
+      command: (e) => {
+        handleRedirect('/leaderboard')
+      }
+    }
+  ]
+  const history = useHistory()
+  const handleRedirect = (redirect) => {
+    history.push(redirect)
   }
-  showConfirm = () => {
-    this.toastBC.show({ severity: 'warn', sticky: true, content: (
+  const showConfirm = () => {
+    toastBC.current.show({ severity: 'warn', sticky: true, content: (
         <div className="p-flex p-flex-column" style={{flex: '1'}}>
             <div className="p-text-center">
                 <i className="pi pi-exclamation-triangle" style={{fontSize: '3rem'}}></i>
@@ -46,49 +48,52 @@ class Navbar extends Component {
                     <Button type="button" 
                       label="Yes" 
                       className="p-button-success" 
-                      onClick={this.handleLogout} />
+                      onClick={handleLogout} />
                 </div>
                 <div className="p-col-6">
                     <Button type="button" 
                       label="No" 
                       className="p-button-secondary" 
-                      onClick={() => this.toastBC.clear()} />
+                      onClick={() => toastBC.current.clear()} />
                 </div>
             </div>
         </div>
     ) });
   }
-  handleLogout = () => {
-    const { dispatch } = this.props
+  const handleLogout = () => {
+    const { dispatch } = props
     
-    this.toastBC.clear()
+    toastBC.current.clear()
     // Nullify auther user to mimic logout
     dispatch(setAuthedUser(null))
   }
-  render () {
-    const start = <img alt="logo" src="/logo.png" height="40" className="p-mr-2"></img>
-    const end = this.props.username 
-      && (
+  const getLogo = () => {
+    return (
+      <img alt="logo" src="/logo.png" height="40" className="p-mr-2"></img>
+    )
+  }
+  const getUsername = () => {
+    return props.username !== null 
+      ? (
         <div className="p-grid p-align-center vertical-container p-justify-end">
           <div className="p-col">
-            <span className="p-text-bold p-text-nowrap p-text-truncate">{'Hello, ' + this.props.username}</span>
+            <span className="p-text-bold p-text-nowrap p-text-truncate">{'Hello, ' + props.username}</span>
           </div>
           <div className="p-col">
             <Button 
               icon="pi pi-power-off" 
               className="p-button-rounded p-button-danger p-button-outlined p-button-sm"
-              onClick={this.showConfirm} />
+              onClick={showConfirm} />
           </div>
         </div>
-      )
-
-    return (
-      <Fragment>
-        <Toast ref={(el) => this.toastBC = el} position="bottom-center" />
-        <Menubar model={this.items} start={start} end={end} />
-      </Fragment>
-    )
+      ) : null
   }
+  return (
+    <Fragment>
+      <Toast ref={toastBC} position="bottom-center" />
+      <Menubar model={items} start={getLogo} end={getUsername} />
+    </Fragment>
+  )
 }
 
 export default withRouter(connect((state) =>
